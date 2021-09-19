5c623e5aef7441419d7407bd4e93cb11ba743392
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-09-19 16:27:44 +0530

    Changed Title Programatically

--- ios-swift-navigation/ViewController.swift
+++ ios-swift-navigation/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Change Title Programatically
+        self.title = "Changed Title Programatically"
     }
 
 
